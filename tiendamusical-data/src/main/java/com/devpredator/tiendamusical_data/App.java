577ea7a5c6a3f534179f1af69be2045ce7a47e2f
karialquicira2 <97080498+karialquicira2@users.noreply.github.com>
2022-09-04 19:19:19 -0500

    Versión inicial del proyecto final de tienda musical

---
+++ tiendamusical-data/src/main/java/com/devpredator/tiendamusical_data/App.java
@@ -0,0 +1,13 @@
+package com.devpredator.tiendamusical_data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tiendamusical-services/src/main/java/com/devpredator/tiendamusical_services/App.java
@@ -0,0 +1,13 @@
+package com.devpredator.tiendamusical_services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
